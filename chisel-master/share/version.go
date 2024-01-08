2ebf121c32fbabd6b2333f2ba4125ad522beee20
Charllmo <128298450+Charllmo@users.noreply.github.com>
2024-01-08 10:26:09 +0900

    Update version.go

--- chisel-master/share/version.go
+++ chisel-master/share/version.go
@@ -4,6 +4,6 @@ package chshare
 //incompatible changes are made, this will
 //be incremented to signify a protocol
 //mismatch.
-var ProtocolVersion = "asdf-3"
+var ProtocolVersion = "asdf-v3"
 
 var BuildVersion = "0.0.0-src"
